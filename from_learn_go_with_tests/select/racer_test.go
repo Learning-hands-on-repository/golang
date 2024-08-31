bd1c8c3debc99858a0a94f9882e91492b286d9cb
narawit-tub <narawit.tub@outlook.co.th>
2024-08-31 11:45:41 +0700

    learn about using httptest to create httpserver mock

---
+++ from_learn_go_with_tests/select/racer_test.go
@@ -0,0 +1,32 @@
+package selectgo
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+}
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(2 * time.Millisecond) 
+	fastServer := makeDelayedServer(time.Microsecond) 
+	defer slowServer.Close()
+	defer fastServer.Close()	
+	
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
