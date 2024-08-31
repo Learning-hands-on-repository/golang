211519666467b8a2b157bd70f500ad180a37631b
narawit-tub <narawit.tub@outlook.co.th>
2024-08-31 15:43:52 +0700

    learning Reflection, the way programming language can inspect their own datastructure of variable

---
+++ from_learn_go_with_tests/reflection/reflection.go
@@ -0,0 +1,35 @@
+package reflection
+
+import "reflect"
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+	// field := val.Field(0)
+	// fn(field.String())
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
+		}
+	}
+}
